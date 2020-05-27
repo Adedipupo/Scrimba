b1b3e79e93ce3eda894286ec0c1caabb4c7a01dc
Adedipupo <bayonleolaluwa@gmail.com>
2020-05-27 02:11:12 +0100

    wip

--- todo/src/App.js
+++ todo/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import todosData from './todosData';
 
 function App() {
-  const todoItems = todosData.map
+  const todoItems = todosData.map(item => <TodoItem />)
   return (
     <div className="todo-list">
       <TodoItem />
