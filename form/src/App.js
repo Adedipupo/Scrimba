c2befad71fd56746e94b1fa1870da20f8ea577e5
Adedipupo <bayonleolaluwa@gmail.com>
2020-06-17 02:26:58 +0100

    forms

--- form/src/App.js
+++ form/src/App.js
@@ -5,11 +5,16 @@ class App extends React.Component {
   constructor(){
     super()
     this.state = {}
+
+    this.handleChange = this.handleChange.bind(this)
+  }
+  handleChange(){
+
   }
   render(){
   return (
    <form>
-     <input type="text" placeholder="firstname" />
+     <input type="text" placeholder="firstname" onChange={this.handleChange}/>
    </form>
   );
   }
